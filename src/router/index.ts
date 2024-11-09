c8a31d0dc002e304f9c3fba31dadd106bd61f741
izmi.komar <komar.izmi@gmail.com>
2024-11-10 05:46:24 +0700

    refactor: migrate router from JavaScript to TypeScript and update package manager in package.json

--- src/router/index.ts
+++ src/router/index.ts
@@ -116,7 +116,6 @@ const router = createRouter({
       name: 'notfound',
       component: () => import('@/views/pages/NotFound.vue')
     },
-
     {
       path: '/auth/login',
       name: 'login',
